3d20995f0b557535c369605e851524b08e44ad9d
Leonard <leonardglez12485@gmail.com>
2025-06-16 13:46:27 -0300

    Add Person class and update Task class by removing IsCompleted property and related methods

---
+++ src/ToDoApp.Domain/Person.cs
@@ -0,0 +1,10 @@
+namespace ToDoApp.Domain;
+
+public class Person : BaseEntity
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
--- src/ToDoApp.Domain/Task.cs
+++ src/ToDoApp.Domain/Task.cs
@@ -1,4 +1,5 @@
-using System.ComponentModel.DataAnnotations;
+
+using System.Reflection.Metadata;
 
 namespace ToDoApp.Domain;
 
@@ -7,13 +8,6 @@ public class Task : BaseEntity
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string status { get; set; } = "Pending"; 
-    public bool IsCompleted { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? CompletedAt { get; set; }
-
-    public void MarkAsCompleted()
-    {
-        IsCompleted = true;
-        CompletedAt = DateTime.UtcNow;
-    }
 }
