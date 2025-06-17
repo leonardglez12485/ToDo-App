74af457ecb5ef3c13306ffc6f2d459daa84c5720
Leonard <leonardglez12485@gmail.com>
2025-06-17 01:17:23 -0300

    Implement CreatePerson endpoint in PersonController with validation and response handling

---
+++ src/ToDoApp.API/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Aplication.DTOs.Person;
+using ToDoApp.Aplication.Persons.PersonCreate;
+using static ToDoApp.Aplication.Persons.PersonCreate.PersonCreateCommand;
+
+namespace ToDoApp.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PersonController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public PersonController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PersonResponse>> CreatePerson([FromBody] PersonCreateRequest personCreateRequest)
+    {
+        if (personCreateRequest is null)
+        {
+            return BadRequest("Person create request cannot be null");
+        }
+        var command = new PersonCreateCommandRequest(personCreateRequest);
+        var response = await _sender.Send(command);
+        if (response is null)
+        {
+            return BadRequest("Failed to create person.");
+        }
+        // return response;
+        return CreatedAtAction(nameof(CreatePerson), new { id = response.Id }, response);
+    }
+        
+}
